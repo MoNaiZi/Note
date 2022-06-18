36c31b49c24aff02251bbf949d0753682221dd59
MoNaiZi <358665514@qq.com>
2022-06-18 13:50:42 +0800

    使用sass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -102,10 +102,13 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style lang="scss" scoped>
+.hello {
+  h3 {
+    margin: 40px 0 0;
+  }
 }
+
 ul {
   list-style-type: none;
   padding: 0;
