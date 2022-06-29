0a7ac914f3bb4dd11791751eb7e917fc931a2ddb
MoNaiZi <358665514@qq.com>
2022-06-29 16:54:14 +0800

    Update menu.vue

--- src/views/menu.vue
+++ src/views/menu.vue
@@ -41,20 +41,6 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.drag_main {
-  -webkit-app-region: drag;
-  position: absolute;
-  left: 0px;
-  right: 0px;
-  bottom: 0px;
-  top: 0px;
-}
-.drag {
-}
-.content {
-  width: 100%;
-  height: 100%;
-}
 .main:hover {
   box-shadow: 0 0 4px #cbcbcb;
   //   -webkit-app-region: drag;
