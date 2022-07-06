48e20c504ab841524ddc63977d059e595e995194
MoNaiZi <358665514@qq.com>
2022-07-06 11:42:39 +0800

    修复同一个应用会被打开多个

--- src/background.js
+++ src/background.js
@@ -15,7 +15,22 @@ protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
 
-// ipcMain.on('set-title', mainProcess.windowsOn)
+
+//关闭当前的应用
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) {
+  app.quit()
+}
+app.on('second-instance', (event, commandLine, workingDirectory) => {
+  // 输出从第二个实例中接收到的数据
+  // 有人试图运行第二个实例，我们应该关注我们的窗口
+  let myWindow = global.mainWin
+  if (myWindow) {
+    if (myWindow.isMinimized()) myWindow.restore()
+    myWindow.focus()
+  }
+})
+
 
 
 //此方法将在Electron完成后调用
@@ -32,6 +47,7 @@ app.on('ready', async () => {
   }
 
   createWindow()
+
 })
 
 async function createWindow() {
